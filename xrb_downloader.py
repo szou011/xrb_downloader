e8a1445aef91983e7a7a20ec9051bb770fd3549b
szou <zou.stephen@gmail.com>
2019-04-25 20:40:21 +1200

    include logging

--- xrb_downloader.py
+++ xrb_downloader.py
@@ -1,30 +1,31 @@
-from lxml import html 
+from lxml import html
 import requests
 import pathlib
+import logging
+import pprint
+
+logging.basicConfig(level=logging.DEBUG)
 
 xrb_url = 'https://www.xrb.govt.nz'
 
+standards_list = {
+        1:"for-profit-entities",
+        2:"not-for-profit",
+        3:"public-sector"
+        }
+
 # to decide which standard to download
-print("""
-        1) For profit standards
-        2) Non-for-profit standards
-        3) Public sector standards
-        """)
+print("Please select the accounting standards to be downloaded:")
+pprint.pprint(standards_list)
 
-result = input('Please choose: ')
-
-if eval(result) == 1:
-    suffix_url = '/accounting-standards/for-profit-entities/'
-elif eval(result) ==2:
-    suffix_url = ''
-elif eval(result) ==3:
-    suffix_url = ''
-else:
+try:
+    selected_standards = standards_list[eval(input('Please input (1, 2 or 3): '))]
+except:
     exit()
 
-html_response = requests.get(xrb_url + suffix_url)
+html_response = requests.get(xrb_url + "/" + selected_standards)
 
-tree = html.fromstring(html_response.content) 
+tree = html.fromstring(html_response.content)
 
 # get all the titles for the accounting standards
 titles = tree.xpath('//li/div/a[@class="title"]/text()')
@@ -35,11 +36,13 @@ for title in titles:
     expr = '//a[text()="' + title + '"]/@href'
     link = xrb_url + tree.xpath(expr)[0]
     standards[title] = link
-    
+
+logging.debug(pprint.pformat(standards))
+
 # create directories for each standard
-xrb_path = pathlib.Path('./xrb/')
+dl_path = pathlib.Path('./xrb/')
 for title in titles:
-    sub_path = xrb_path / title.replace(" ", "_")
+    sub_path = dl_path / selected_standards / title.replace(" ", "_")
 #    sub_path.mkdir(parents=True, exist_ok=True)
     print(sub_path)
 
